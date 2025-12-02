1f370d6393fc71054c01818465adf159c5ddb51f
Shalev Mazuz <shalevmazuz@gmail.com>
2025-12-02 11:53:18 +0200

    feat: add NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import ProjectsSection from './components/ProjectsSection'
 
 function App() {
   return (
     <div className="app-container">
       <Hero />
+      <ProjectsSection />
     </div>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-logo">Lumina.</div>
+            <div className="nav-links">
+                <a href="#projects" className="nav-link">Work</a>
+                <a href="#contact" className="nav-link">Contact</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
