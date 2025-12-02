4c45796d66a833bb29dcd5f47f22d4b541430cf0
Shalev Mazuz <shalevmazuz@gmail.com>
2025-12-02 15:14:18 +0200

    feat: app changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import ProjectsSection from './components/ProjectsSection'
 import Navbar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <ProjectsSection />
+      <Footer />
     </div>
   )
 }
