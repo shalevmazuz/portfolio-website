a4cd276fe9ec6abc93438899643b771a9f1c3a35
Shalev Mazuz <shalevmazuz@gmail.com>
2025-12-03 12:51:08 +0200

    feat: modify download button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,25 @@
 import './Hero.css'
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 const Hero = () => {
     return (
         <section id="hero" className="hero-section">
             <div className="hero-content">
-                <h1>
-                    Shalev Mazuz
-                </h1>
+                <h1>Shalev Mazuz</h1>
                 <p className="hero-subtitle">
                     I am a Computer Science graduate from Tel Aviv University seeking entry-level opportunities in software
                     development. I bring a strong foundation in programming and a desire to apply my skills to real-world challenges while
                     contributing to innovative projects.
                 </p>
-                <a href="public\Resume-Shalev Mazuz.pdf" className="cta-button" download="Resume-Shalev Mazuz.pdf">Download Resume</a>
+                <a
+                    href="/Resume-Shalev Mazuz.pdf"
+                    className="cta-button"
+                    download="Resume-Shalev Mazuz.pdf"
+                >
+                    <FileDownloadIcon />
+                    Download Resume
+                </a>
+
             </div>
         </section>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav className="navbar">
             <div className="nav-logo">SHALEV MAZUZ</div>
             <div className="nav-links">
-                <a href="#projects" className="nav-link">Work</a>
+                <a href="#projects" className="nav-link">Projects</a>
                 <a href="#contact" className="nav-link">Contact</a>
             </div>
         </nav>
