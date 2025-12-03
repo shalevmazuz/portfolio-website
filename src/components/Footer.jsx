418c8b79194566e349f883e786cf1ce07528b119
Shalev Mazuz <shalevmazuz@gmail.com>
2025-12-03 13:09:52 +0200

    feat: updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import './Footer.css'
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 const Footer = () => {
     return (
@@ -7,8 +9,13 @@ const Footer = () => {
                 <h2>Let's build something great.</h2>
                 <p>Open for new opportunities.</p>
                 <a href="mailto:shalevmazuz@gmail.com" className="email-link">
+                    <EmailIcon fontSize='small' />
                     shalevmazuz@gmail.com
                 </a>
+                <div className="phone-number">
+                    <PhoneIcon fontSize='small' />
+                    +972 50-951-8297
+                </div>
                 <div className="footer-copyright">
                     © {new Date().getFullYear()} Shalev Mazuz. All rights reserved.
                 </div>
