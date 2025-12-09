9cbc1f3b813d0daf2aabb64e2c476af4de5b695c
Shalev Mazuz <shalevmazuz@gmail.com>
2025-12-09 11:55:42 +0200

    feat: footer modifications

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     shalevmazuz@gmail.com
                 </a>
                 <div className="phone-number">
-                    <a href='https://www.linkedin.com/in/shalev-mazuz-781325353/' className='email-link'>
+                    <a href='https://www.linkedin.com/in/shalev-mazuz-781325353/' target='_blank' className='email-link'>
                         <FaLinkedin />
                         LinkedIn
                     </a>
